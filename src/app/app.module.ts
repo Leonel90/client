f2570bf9e0326533b782d8eae5a55b8d77106a78
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2022-12-21 09:22:39 -0500

    Creación de nuevos componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,32 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PagesComponent } from './pages/pages.component';
+import { NoFoundComponent } from './noFoundPages/no-found/no-found.component';
+import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { ProductComponent } from './pages/product/product.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PagesComponent,
+    NoFoundComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    RegisterComponent,
+    CategoryComponent,
+    ProductComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
