e8727178975c305e3c587f0858d655fae2f6be47
Leonel Sangolquiza <97122219+Leonel90@users.noreply.github.com>
2023-01-22 14:50:29 -0500

    detelete con mapeo

--- src/app/services/product-http-service.service.ts
+++ src/app/services/product-http-service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { CreateProductDto, ProductModel, UpdateProductDto } from '../entities/product.model';
 
 @Injectable({
@@ -13,28 +13,30 @@ export class ProductHttpServiceService {
 
   }
 
-  getAll():Observable<ProductModel[]>{
+  getAll(): Observable<ProductModel[]> {
     const url = `${this.API_UR}`;
     return this.httpClient.get<ProductModel[]>(url);
   }
 
-  getOne(id: ProductModel['id']):Observable<ProductModel[]>{
+  getOne(id: ProductModel['id']): Observable<ProductModel[]> {
     const url = `${this.API_UR}/${id}`;
     return this.httpClient.get<ProductModel[]>(url);
   }
 
-  store(product: CreateProductDto):Observable<ProductModel>{
+  store(product: CreateProductDto): Observable<ProductModel> {
     const url = `${this.API_UR}`;
     return this.httpClient.post<ProductModel>(url, product);
   }
 
-  update(id: ProductModel['id'], product: UpdateProductDto):Observable<ProductModel>{
+  update(id: ProductModel['id'], product: UpdateProductDto): Observable<ProductModel> {
     const url = `${this.API_UR}/${id}`;/*Recuest hacemos la peticion*/
     return this.httpClient.put<ProductModel>(url, product);/*Respons recibimos la respuesta*/
   }
 
-  destroy(id: ProductModel['id']):Observable<ProductModel>{
+  destroy(id: ProductModel['id']): Observable<any> {
     const url = `${this.API_UR}/${id}`;
-    return this.httpClient.delete<ProductModel>(url);
+    return this.httpClient.delete<any>(url).pipe(map((response: { rta: boolean }) => {
+      return response.rta;
+    }));
   }
 }
